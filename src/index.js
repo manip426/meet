2722d68a4d43e09affc72332ea2dadcd2045b07e
Manpreet Dhaliwal <manip426@gmail.com>
2021-08-24 17:50:30 -0700

    Added images

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import * as atatus from 'atatus-spa';
-atatus.config('9f3cd62cd10541eb9baa09ef1c3095a5').install();
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,7 +14,12 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+//serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+atatus.config('9f3cd62cd10541eb9baa09ef1c3095a5').install();
